1a09eb753a3030d7d2f4801f3cc078dfbc0d32e3
Arjun-Joshi20 <83960724+Arjun-Joshi20@users.noreply.github.com>
2021-05-16 22:13:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,69 @@
+import tkinter
+import tkinter.messagebox
+import pickle
+
+root = tkinter.Tk()
+root.title('To Do List App')
+
+def add_task():
+    task = entry_task.get() #Get() gives the text of whatever we have entered.
+    if task != '':
+        listbox_tasks.insert(tkinter.END, task)
+        entry_task.delete()
+    else:
+        tkinter.messagebox.showwarning(title='Warning', message='You must enter a task')
+
+def delete_task():
+    try:
+        task_index = listbox_tasks.curselection()[0]
+        listbox_tasks.delete(task_index)
+    except:
+        tkinter.messagebox.showwarning(title='Warning',  message='You must select a task')
+
+
+def load_task():
+    try:
+        tasks = pickle.load(open("tasks.dat", "rb"))
+        listbox_tasks.delete(0, tkinter.END)
+        for task in tasks:
+            listbox_tasks.insert(tkinter.END, task)
+    except:
+        tkinter.messagebox.showwarning(title="Warning!", message="Cannot find tasks.dat.")
+
+def save_task():
+    tasks = listbox_tasks.get(0, listbox_tasks.size())
+    pickle.dump(tasks, open("tasks.dat", 'wb'))
+#Create Gui
+
+frame_tasks = tkinter.Frame(root)
+frame_tasks.pack()
+
+listbox_tasks = tkinter.Listbox(frame_tasks, bg='yellow', height=30, width=100)
+listbox_tasks.pack(side=tkinter.LEFT)
+
+entry_task = tkinter.Entry(root, width=50)
+entry_task.pack()
+
+scrollbar_tasks = tkinter.Scrollbar(frame_tasks)
+scrollbar_tasks.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+
+listbox_tasks.config(yscrollcommand=scrollbar_tasks.set)
+scrollbar_tasks.config(command=listbox_tasks.yview)
+
+button_add_task = tkinter.Button(root, text="Add task", width=48, command=add_task)
+button_add_task.pack()
+
+button_delete_task = tkinter.Button(root, text="Delete task", width=48, command=delete_task)
+button_delete_task.pack()
+
+button_load_task = tkinter.Button(root, text="Load task", width=48, command=load_task)
+button_load_task.pack()
+
+file_name = tkinter.Entry(root, width=50)
+file_name.pack()
+
+button_save_task = tkinter.Button(root, text="Save tasks", width=48, command=save_task)
+button_save_task.pack()
+
+
+root.mainloop()
